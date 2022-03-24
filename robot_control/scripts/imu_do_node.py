cd5ec03c34cd4261c786d4a08cfde9fb20100d6f
225ceV <jiakun_jin@163.com>
2022-03-24 13:04:11 +0000

    改正了大转向角下失去平稳的错误 	修改：     robot_control/scripts/imu_do_node.py
    	robot_control/scripts/__pycache__/imu_do_node.cpython-38.pyc
    	robot_control/scripts/__pycache__/lqr_control1.cpython-38.pyc
    	robot_control/scripts/__pycache__/multi_receive.cpython-38.pyc
    	robot_control/scripts/__pycache__/online_lqr1.cpython-38.pyc
    	robot_control/scripts/cmd.py
    	robot_control/scripts/modern_robotics/

--- robot_control/scripts/imu_do_node.py
+++ robot_control/scripts/imu_do_node.py
@@ -15,13 +15,8 @@ class imu_do_node:
         rospy.spin()
     def imu_get(self, imu_data):
         o = robot_orientation()
-        '''x = float(imu_data.orientation.x) #q1
-        y = float(imu_data.orientation.y) #q2
-        z = float(imu_data.orientation.z) #q3
-        w = float(imu_data.orientation.w) #q0
-        o.theta = - math.atan2(2*(w*x+y*z),1-2*(x**2+y**2))
-        o.delta = math.atan2(2*(w*z+x*y),1-2*(y**2+z**2))'''
-        q1 = float(imu_data.orientation.x) #q1
+
+        '''q1 = float(imu_data.orientation.x) #q1
         q2 = float(imu_data.orientation.y) #q2
         q3 = float(imu_data.orientation.z) #q3
         q0 = float(imu_data.orientation.w) #q0
@@ -35,7 +30,32 @@ class imu_do_node:
         o.delta=math.atan(ry0/math.sqrt(ry1**2+ry2**2))
         
         o.dtheta=-imu_data.angular_velocity.x
-        o.ddelta=-imu_data.angular_velocity.z
+        o.ddelta=-imu_data.angular_velocity.z'''
+
+        x = float(imu_data.orientation.x) #q1
+        y = float(imu_data.orientation.y) #q2
+        z = float(imu_data.orientation.z) #q3
+        w = float(imu_data.orientation.w) #q0
+
+        angle = np.arccos(w) * 2
+        temp = np.sin(angle/2)
+        kx = x / temp
+        ky = y / temp
+        kz = z / temp
+        so3mat = mr.VecToso3(np.array([kx, ky, kz])*angle)
+        R = mr.MatrixExp3(so3mat)
+
+        ry0 = R[0,1]
+        ry1 = R[1,1]
+        ry2 = R[2,1]
+        
+        o.theta=math.atan(-ry2/math.sqrt(ry0**2+ry1**2))
+        o.delta=math.atan(ry0/math.sqrt(ry1**2+ry2**2))
+        ws = np.array([[imu_data.angular_velocity.x], [imu_data.angular_velocity.y], [imu_data.angular_velocity.z]])
+        wb = R.T.dot(ws)
+
+        o.dtheta = -wb[0,0]
+        o.ddelta = -wb[2,0]
         # 时间戳
         o.header.stamp = rospy.Time.now()
         # 发布
